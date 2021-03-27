703e14e1c834c71de9925c8f58ff20f28b1e922f
Bruno Carneiro <bruno.ferreira@trf1.jus.br>
2021-03-27 17:07:46 -0300

    setup

---
+++ src/main/java/br/com/codethebasics/telepathic/TelepathicApplication.java
@@ -0,0 +1,13 @@
+package br.com.codethebasics.telepathic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelepathicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelepathicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/codethebasics/telepathic/TelepathicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codethebasics.telepathic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelepathicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
