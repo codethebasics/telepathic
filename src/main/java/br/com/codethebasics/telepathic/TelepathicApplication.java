a0af5cb792cf3443f38c3e9c7de61c5e15de346c
Bruno Carneiro <bruno.ferreira@trf1.jus.br>
2021-04-01 13:55:46 -0300

    modularizando

--- src/main/java/br/com/codethebasics/telepathic/TelepathicApplication.java
+++ src/main/java/br/com/codethebasics/telepathic/TelepathicApplication.java
@@ -1,51 +1,22 @@
 package br.com.codethebasics.telepathic;
 
-import br.com.codethebasics.telepathic.nyt.model.ArchiveResponse;
-import br.com.codethebasics.telepathic.nyt.service.NytArchiveService;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.request.SendPhoto;
-import com.pengrad.telegrambot.response.SendResponse;
+import br.com.codethebasics.telepathic.bot.TelepathicBot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelepathicApplication {
 
+    private static TelepathicBot telepathicBot;
+
+    @Autowired
+    public TelepathicApplication(TelepathicBot telepathicBot) {
+        this.telepathicBot = telepathicBot;
+    }
+
     public static void main(String[] args) {
-
-        NytArchiveService nytArchiveService = new NytArchiveService();
-
-        TelegramBot bot = new TelegramBot("1688657217:AAFRN6cxwaLKe2bjsRem-iUwSvPjySx3nw8");
-        bot.setUpdatesListener(updates -> {
-            updates.forEach(update -> {
-                if (update.message() != null) {
-                    long chatId = update.message().chat().id();
-                    try {
-                        String text = update.message().text();
-                        if (text.startsWith("::nyt-archive-")) {
-                            String textFilter = text.replace("::nyt-archive-", "");
-                            String[] params = textFilter.split("/");
-                            if (params.length != 2) {
-                                bot.execute(new SendMessage(chatId, "Parâmetros insuficientes."));
-                            }
-                            bot.execute(new SendMessage(chatId, "Sua solicitação está sendo procesada. Aguarde..."));
-                            ArchiveResponse response = nytArchiveService.get(Integer.parseInt(params[0]), Integer.parseInt(params[1]));
-                            bot.execute(new SendPhoto(chatId, "https://www.nytimes.com/" + response.getResponse().getDocs()[0].getMultimedia()[0].getUrl()));
-                            bot.execute(new SendMessage(chatId, response.getResponse().getDocs()[0].getAbstractAttr()));
-                        }
-                        else {
-                            bot.execute(new SendMessage(chatId, "Comando inválido."));
-                        }
-                    }
-                    catch (Exception e) {
-                        bot.execute(new SendMessage(chatId, "Desculpe, não foi possível processar a sua requisição."));
-                    }
-                }
-            });
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-//        SpringApplication.run(TelepathicApplication.class, args);
+        SpringApplication.run(TelepathicApplication.class, args);
+        telepathicBot.listenForUpdates();
     }
 }
--- src/main/java/br/com/codethebasics/telepathic/bot/TelepathicBot.java
+++ src/main/java/br/com/codethebasics/telepathic/bot/TelepathicBot.java
@@ -1,15 +1,89 @@
 package br.com.codethebasics.telepathic.bot;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Component
 public class TelepathicBot {
 
-    private String name;
-    private List<String> actions;
+    @Value("${telegram.bot.token}")
+    private String token;
 
+    private TelegramBot bot;
+
+    public TelepathicBot() {
+        initBot();
+    }
+
+    private void initBot() {
+        this.bot = new TelegramBot("1688657217:AAFRN6cxwaLKe2bjsRem-iUwSvPjySx3nw8");
+    }
+
+    /**
+     * Listening for chat updates
+     */
+    public void listenForUpdates() {
+        try {
+            if (this.bot == null) {
+                throw new Exception("There's no bot started.");
+            }
+            this.bot.setUpdatesListener(this::processUpdates);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Processing a list of updates
+     * @param updates
+     */
+    private int processUpdates(List<Update> updates) {
+        try {
+            updates.forEach(this::processUpdate);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }
+        catch (Exception e) {
+            return UpdatesListener.CONFIRMED_UPDATES_NONE;
+        }
+    }
+
+    /**
+     * Processing an update
+     * @param update
+     */
+    private void processUpdate(Update update) {
+        try {
+            String receivedMessage = update.message().text();
+            sendMessage("You said '" + receivedMessage + "'", update.message().chat().id());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            sendMessage("Cannot process update.", update.message().chat().id());
+        }
+    }
+
+    /**
+     * Send a message to a chat
+     * @param text
+     * @param chatId
+     */
+    private void sendMessage(String text, long chatId) {
+        try {
+            bot.execute(new SendMessage(chatId, text));
+        }
+        catch (Exception e) {
+            bot.execute(new SendMessage(chatId, "Cannot send message."));
+        }
+    }
 }
